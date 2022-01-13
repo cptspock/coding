a8457755c7063be98a12f8238f2e2abe7f3be756
cptspock <dreamday@gmail.com>
2022-01-13 10:45:25 -0800

    added

--- src/solutions/PlusOne.java
+++ src/solutions/PlusOne.java
@@ -7,22 +7,22 @@ Increment the large integer by one and return the resulting array of digits.
 package solutions;
 
 public class PlusOne {
-    public int[] plusOne(int[] digits) {
-    	int len = digits.length;
-        for (int i = len - 1; i >=0; i--) {
-        	if (digits[i] < 9) {
-        		digits[i]++;
-        		return digits;
-        	}
-        	digits[i] = 0;
-        }
-        int[] newNumber = new int[len + 1];
-    	newNumber[0] = 1;
-    	return newNumber;
-    }
-    
+	public int[] plusOne(int[] digits) {
+		int len = digits.length;
+		for (int i = len - 1; i >= 0; i--) {
+			if (digits[i] < 9) {
+				digits[i]++;
+				return digits;
+			}
+			digits[i] = 0;
+		}
+		int[] newNumber = new int[len + 1];
+		newNumber[0] = 1;
+		return newNumber;
+	}
+
 	public static void main(String[] args) {
-		int[] res = new PlusOne().plusOne(new int[] {9,9,9});
+		int[] res = new PlusOne().plusOne(new int[] { 9, 9, 9 });
 		for (int num : res) {
 			System.out.println(num);
 		}
